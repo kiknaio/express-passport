c595ce34f16e2991c8c90ecbed4b98abc1dd2ac8
George Kiknadze <g.kiknadze@hotmail.com>
2016-09-28 17:10:27 +0200

    No change here, just removed passport small snippets

--- app.js
+++ app.js
@@ -5,10 +5,38 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const flash = require('connect-flash');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./model/user');
+
+
 
 const routes = require('./routes');
 
 const app = express();
+
+// Initialize Passport local strategy
+passport.use(new LocalStrategy(
+  function(username, password, cb) {
+    User.findOne({ username: username }, function(err, user) {
+      if(err) { return cb(err); }
+      if(!user) { return cb(null, false); }
+      if(user.password != password) { return cb(null, false); }
+      return cb(null, user);
+    });
+  }
+));
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user.id);
+});
+
+passport.deserializeUser(function(id, cb) {
+  db.users.findById(id, function (err, user) {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
 
 mongoose.connect('mongodb://localhost:27017/test');
 
@@ -22,9 +50,12 @@ app.use(cookieParser());
 app.use(session({
   secret: 'myfirstexpressappwithpassport',
   resave: true,
-  saveUninitialized: true
+  saveUninitialized: false
 }));
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(routes);
 
